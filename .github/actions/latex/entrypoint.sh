b1a535cee97a0451d97180b000d6ff4c6c90658c
Ryu Yamada <yamadaryutokai@yahoo.co.jp>
2019-12-07 18:34:20 +0900

    update : github actions

--- .github/actions/latex/entrypoint.sh
+++ .github/actions/latex/entrypoint.sh
@@ -2,15 +2,15 @@
 set -eux
 
 # build pdf (change if necessary)
-pdflatex main.tex
+ptex2pdf -l -ot -kanji=utf8 main.tex
 
 # create release
-res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/tychy/jackson_electrodynamics_jp/releases \
+res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
 -d "
 {
-  \"tag_name\": \"v$GITHUB_SHA\",
+  \"tag_name\": \"$(echo ${GITHUB_REF:10})\",
   \"target_commitish\": \"$GITHUB_SHA\",
-  \"name\": \"v$GITHUB_SHA\",
+  \"name\": \"main.pdf $(echo ${GITHUB_REF:10})\",
   \"draft\": false,
   \"prerelease\": false
 }"`
@@ -19,6 +19,6 @@ res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com
 rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
 
 # upload built pdf
-curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/tychy/jackson_electrodynamics_jp/releases/${rel_id}/assets?name=main.pdf\
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=main.pdf\
   --header 'Content-Type: application/pdf'\
   --upload-file main.pdf
